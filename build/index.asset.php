6269ee70f9f6ffb541bec6aab170124223e0ab74
JuanMa <juanma.garrido@gmail.com>
2025-04-16 11:45:25 +0100

    Refactor Word Switcher Block by removing unused alignment properties in block.json and updating asset version in index.asset.php for consistency. This streamlines the block's configuration and ensures proper versioning.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n', 'wp-rich-text'), 'version' => 'd1d58c9feac9ba46a534');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n', 'wp-rich-text'), 'version' => '332e505134bed90c36e1');
